a535e4feb78d6e76cfaedc8212347d1f4019c895
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-08-29 11:20:05 +0700

    kategori

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function hitungBmi(){
     let bb = document.getElementById("bb").value
     let tb = document.getElementById("tb").value
+   let katagori = ""
 
     tinggiMeter = tb/100
     bmi = bb/Math.pow(tinggiMeter,2)
 
-    console.log(bmi)
+    if(bmi <= 18.5){
+        katagori = "Underweight";
+    } else if (bmi <= 24.9){
+        katagori = "Normal weight";
+    } else if(bmi <= 29.9){
+        katagori = "Overweight";
+    } else{
+        katagori = "Obesity";
+    }
+    
+    console.log(bmi + katagori)
 }
